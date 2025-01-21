ba9e2310fb6aea57619a508204d8fcc094e91574
afaz <abolfazlmajidi100@gmail.com>
2025-01-21 14:42:31 +0330

    add almalinux repository

--- fix-centos7-repository/main.sh
+++ fix-centos7-repository/main.sh
@@ -7,7 +7,9 @@ echo "Creating new yum.repos.d directory..."
 mkdir /etc/yum.repos.d
 
 echo "Downloading and placing CentOS-Base.repo..."
-wget -O /etc/yum.repos.d/CentOS-Base.repo "https://raw.githubusercontent.com/imafaz/awesome-scripts/refs/heads/main/fix-centos7-repository/CentOS-Base.repo"
+wget -O /etc/yum.repos.d/CentOS-Base.repo "https://el7.repo.almalinux.org/centos/CentOS-Base.repo"
+
+yum clean all --verbose
 
 echo "Repositories successfully replaced."
 echo "Now you can use the 'yum' command!"
